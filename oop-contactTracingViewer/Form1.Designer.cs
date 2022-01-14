5c1ec5fc605ef20a653b761452e4665e08af5b8b
benedictfernando <fernandobenedict619@gmail.com>
2022-01-14 18:00:52 +0800

    add & design table layout panel

--- oop-contactTracingViewer/Form1.Designer.cs
+++ oop-contactTracingViewer/Form1.Designer.cs
@@ -29,7 +29,47 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(viewer));
+            this.outerTable = new System.Windows.Forms.TableLayoutPanel();
+            this.innerTable = new System.Windows.Forms.TableLayoutPanel();
+            this.outerTable.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // outerTable
+            // 
+            this.outerTable.ColumnCount = 3;
+            this.outerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.outerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.outerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.outerTable.Controls.Add(this.innerTable, 1, 1);
+            this.outerTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outerTable.Location = new System.Drawing.Point(0, 0);
+            this.outerTable.Name = "outerTable";
+            this.outerTable.RowCount = 3;
+            this.outerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.outerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.outerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.outerTable.Size = new System.Drawing.Size(780, 457);
+            this.outerTable.TabIndex = 0;
+            // 
+            // innerTable
+            // 
+            this.innerTable.ColumnCount = 3;
+            this.innerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.innerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.innerTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.innerTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.innerTable.Location = new System.Drawing.Point(23, 23);
+            this.innerTable.Name = "innerTable";
+            this.innerTable.RowCount = 7;
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28531F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.2853F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.2853F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.2853F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.2853F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.2853F));
+            this.innerTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28816F));
+            this.innerTable.Size = new System.Drawing.Size(734, 411);
+            this.innerTable.TabIndex = 0;
             // 
             // viewer
             // 
@@ -37,15 +77,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(780, 457);
+            this.Controls.Add(this.outerTable);
             this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "viewer";
             this.Text = "Contact Tracing Viewer";
+            this.outerTable.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel outerTable;
+        private System.Windows.Forms.TableLayoutPanel innerTable;
     }
 }
